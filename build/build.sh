02a21d9064572fbc31f01861db946b0f9981ce99
Manjunatha Madana <quic_c_mamanj@quicinc.com>
2021-11-12 16:32:57 +0530

    build.sh: Enable msm8937_lily split lunch
    Add split lunch support for msm8937_lily to work
    with qssi_32go.
    
    Change-Id: I754692e19cdce2045e5d331f49a78d0e4d2008d2

--- build/build.sh
+++ build/build.sh
@@ -188,7 +188,7 @@ if [[ "$TARGET_PRODUCT" == "qssi" ]]; then
     QSSI_ONLY=1
 fi
 
-QSSI_TARGETS_LIST=("holi" "taro" "kalama" "kalama64" "pineapple" "lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "lito" "bengal" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go")
+QSSI_TARGETS_LIST=("holi" "taro" "kalama" "kalama64" "pineapple" "lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "lito" "bengal" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "msm8937_lily")
 VENDOR_64_ONLY_CONFIGS=("qssi_64" "kalama64" "pineapple")
 QSSI_TARGET_FLAG=0
 SKIP_ABI_CHECKS=true
@@ -200,6 +200,9 @@ case "$TARGET_PRODUCT" in
         ;;
     *_32go)
        TARGET_QSSI="qssi_32go"
+        ;;
+    *_lily)
+        TARGET_QSSI="qssi_32go"
         ;;
     *)
         TARGET_QSSI="qssi"
@@ -215,7 +218,7 @@ do
     fi
 done
 
-NON_AB_TARGET_LIST=("qssi_32go" "bengal_32go")
+NON_AB_TARGET_LIST=("qssi_32go" "bengal_32go" "msm8937_lily")
 for NON_AB_TARGET in "${NON_AB_TARGET_LIST[@]}"
 do
     if [ "$TARGET_PRODUCT" == "$NON_AB_TARGET" ]; then
@@ -250,9 +253,9 @@ DIST_DIR="out/dist"
 MERGED_TARGET_FILES="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-target_files.zip"
 LEGACY_TARGET_FILES="$DIST_DIR/${TARGET_PRODUCT}-target_files-*.zip"
 MERGED_OTA_ZIP="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-ota.zip"
-DIST_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "kalama64" "pineapple" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "sdm660_64")
+DIST_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "kalama64" "pineapple" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "sdm660_64" "msm8937_lily")
 VIRTUAL_AB_ENABLED_TARGET_LIST=("kona" "lito" "taro" "kalama" "kalama64" "pineapple" "lahaina")
-DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "kalama64" "pineapple" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal" "bengal_32" "bengal_32go" "sm6150" "sdm660_64")
+DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "kalama64" "pineapple" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal" "bengal_32" "bengal_32go" "sm6150" "sdm660_64" "msm8937_lily")
 DYNAMIC_PARTITIONS_IMAGES_PATH=$OUT
 DP_IMAGES_OVERRIDE=false
 TECHPACK_LIST=("camera_tp" "display_tp" "video_tp" "audio_tp" "sensors_tp" "cv_tp" "xr_tp")
