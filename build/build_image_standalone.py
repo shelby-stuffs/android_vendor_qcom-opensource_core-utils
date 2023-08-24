78eaf330eca28ac4c662f1df74196f8bff08e4f8
Taiyab Haque <quic_thaque@quicinc.com>
2023-08-24 15:54:13 +0530

    pitti: Add target pitti
    Change-Id: If9485d295e1cf164367bcd94de0d43ad4e30d176

--- build/build_image_standalone.py
+++ build/build_image_standalone.py
@@ -322,7 +322,7 @@ def main():
     "qssi"      : ["holi", "taro", "kalama", "lahaina", "sdm710", "sdm845", "msmnile", "sm6150", "kona", "atoll", "trinket", "lito", "bengal", "parrot", "bengal_515", "crow", "anorak"],
     "qssi_32"   : ["bengal_32", "monaco"],
     "qssi_32go" : ["bengal_32go", "msm8937_lily"],
-    "qssi_64"   : ["kalama64", "pineapple", "blair", "sun", "niobe"],
+    "qssi_64"   : ["kalama64", "pineapple", "blair", "sun", "niobe", "pitti"],
   }
 
   if args.target_lunch   in vendor_qssi_mapping_dict['qssi']:
